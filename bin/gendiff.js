0c9cdace21c361fca2a7cb4a38660dd4e1709243
Artem <selfex9496@gmail.com>
2023-02-15 22:35:44 +0700

    add gendiff to Makefile

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,5 +1,7 @@
-const { program } = require('commander');
+#!/usr/bin/env node
+import { Command } from 'commander';
 
+const program = new Command();
 program
   .version('0.0.1', '-v, --vers', 'output the current version')
-  .description('-h', '');
+  .description('-h', 'Compares two configuration files and shows a difference.');
