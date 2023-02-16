fe7863539b0c0d1fec4b16b7f4609e8c7780b67c
Artem <selfex9496@gmail.com>
2023-02-16 21:04:56 +0700

    add option format, add new file.json(1,2)

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,6 +5,7 @@ const program = new Command();
 
 program
   .version('0.0.1')
-  .description('Compares two configuration files and shows a difference.');
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format <type>', 'output format');
 
 program.parse();
