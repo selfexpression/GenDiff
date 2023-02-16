73526de786aca223f0fa64505276313267185963
Artem <selfex9496@gmail.com>
2023-02-17 01:04:09 +0700

    inst lodash, add gendiff function

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,11 +1,17 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
+import genDiff from '../src/index.js';
 
 const program = new Command();
 
 program
-  .version('0.0.1')
+.version('1.0.0')
   .description('Compares two configuration files and shows a difference.')
-  .option('-f, --format <type>', 'output format');
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format <type>', 'output format', 'stylish')
+  .action((filepath1, filepath2) => {
+    const diff = genDiff(filepath1, filepath2);
+    console.log(diff);
+  });
 
-program.parse();
+program.parse(process.argv);
--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
-import { readFileSync } from 'node:fs';
+import { readFileSync } from 'fs';
+import _ from 'lodash';
+
+const genDiff = (data1, data2) => {
+    const unionKeys = _.union(_.keys(data1), _.keys(data2));
+  
+    const result = unionKeys.reduce((acc, key) => {
+      const firstCheck = _.has(data1, key);
+      const secondCheck = _.has(data2, key);
+      if (!firstCheck) {
+        acc[key] = '-';
+      } else if (!secondCheck) {
+        acc[key] = '+';
+      } else if (data1[key] !== data2[key]) {
+        acc[key] = '-';
+      } else {
+        acc[key] = '';
+      }
+  
+      return acc;
+    }, {});
+  
+    return result;
+  };
+
+  export default (filePath1, filePath2) => {
+    const data1 = readFileSync(filePath1, 'utf-8');
+    const data2 = readFileSync(filePath2, 'utf-8');
+    const dataParse1 = JSON.parse(data1);
+    const dataParse2 = JSON.parse(data2);
+    return genDiff(dataParse1, dataParse2);
+  };
