d55d94a3628de1f3df31dedd4a6ee79042fa1210
Artem <selfex9496@gmail.com>
2023-02-17 11:38:11 +0700

    just save

--- src/index.js
+++ src/index.js
@@ -2,25 +2,23 @@ import { readFileSync } from 'fs';
 import _ from 'lodash';
 
 const genDiff = (data1, data2) => {
-    const unionKeys = _.union(_.keys(data1), _.keys(data2));
-  
-    const result = unionKeys.reduce((acc, key) => {
-      const firstCheck = _.has(data1, key);
-      const secondCheck = _.has(data2, key);
-      if (!firstCheck) {
-        acc[key] = '-';
-      } else if (!secondCheck) {
-        acc[key] = '+';
-      } else if (data1[key] !== data2[key]) {
-        acc[key] = '-';
+    const pairs = Object.entries(data1);
+    const pairs2 = Object.entries(data2);
+    const unionPairs = _.unionWith(pairs, pairs2, _.isEqual);
+
+    const result = _.sortBy(unionPairs).map(([key, value]) => {
+      if (!_.has(data1, key)) {
+        return `+ ${key}: ${value}`;
+      } else if (!_.has(data2, key)) {
+        return `- ${key}: ${value}`;
+      } else if (_.has(data1, key) && _.has(data2, key) && data1[value] !== data2[value]) {
+        return `- ${key}: ${value}`;
       } else {
-        acc[key] = '';
+        return `  ${key}: ${value}`;
       }
+    });
   
-      return acc;
-    }, {});
-  
-    return result;
+    return result.join('\n');
   };
 
   export default (filePath1, filePath2) => {
