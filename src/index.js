0e348ca080b1e08ad8b01d12de82df54c446dd8c
Artem <selfex9496@gmail.com>
2023-02-17 21:34:05 +0700

    fix gendiff, add getRawPath

--- src/index.js
+++ src/index.js
@@ -1,29 +1,43 @@
 import { readFileSync } from 'fs';
+import { cwd } from 'process';
+import path from 'path';
 import _ from 'lodash';
 
-const genDiff = (data1, data2) => {
-    const pairs = Object.entries(data1);
-    const pairs2 = Object.entries(data2);
-    const unionPairs = _.unionWith(pairs, pairs2, _.isEqual);
+const getRawData = (filePath) => {
+  const fullPath = path.resolve(cwd(), filePath);
+  return readFileSync(fullPath, 'utf-8');
+};
 
-    const result = _.sortBy(unionPairs).map(([key, value]) => {
-      if (!_.has(data1, key)) {
-        return `+ ${key}: ${value}`;
-      } else if (!_.has(data2, key)) {
-        return `- ${key}: ${value}`;
-      } else if (_.has(data1, key) && _.has(data2, key) && data1[value] !== data2[value]) {
-        return `- ${key}: ${value}`;
-      } else {
-        return `  ${key}: ${value}`;
+const genDiff = (data1, data2) => {
+    const unionKeys = _.union(_.keys(data1), _.keys(data2));
+
+    const result = _.sortBy(unionKeys).reduce((acc, key) => {
+      if (_.has(data1, key) && _.has(data2, key)) {
+        if (data1[key] === data2[key]) {
+          acc.push(`    ${key}: ${data1[key]}`);
+        }
       }
-    });
-  
-    return result.join('\n');
-  };
+      if (_.has(data1, key) && _.has(data2, key)) {
+        if (data1[key] !== data2[key]) {
+          acc.push(`  - ${key}: ${data1[key]}`);
+          acc.push(`  + ${key}: ${data2[key]}`);
+        }
+      }
+      if (_.has(data1, key) && !_.has(data2, key)) {
+          acc.push(`  - ${key}: ${data1[key]}`);
+      }
+      if (!_.has(data1, key) && _.has(data2, key)) {
+          acc.push(`  + ${key}: ${data2[key]}`);
+      }
+      return acc;
+  }, []);
+
+  return result.join('\n');
+};
 
   export default (filePath1, filePath2) => {
-    const data1 = readFileSync(filePath1, 'utf-8');
-    const data2 = readFileSync(filePath2, 'utf-8');
+    const data1 = getRawData(filePath1);
+    const data2 = getRawData(filePath2);
     const dataParse1 = JSON.parse(data1);
     const dataParse2 = JSON.parse(data2);
     return genDiff(dataParse1, dataParse2);
