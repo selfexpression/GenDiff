855a205ff60290fe215bbdab4d248b32712fc358
Artem <selfex9496@gmail.com>
2023-02-18 18:48:11 +0700

    github action check

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,11 +1,13 @@
 #!/usr/bin/env node
+/* eslint-disable no-console */
+/* eslint-disable import/extensions */
 import { Command } from 'commander';
 import genDiff from '../src/index.js';
 
 const program = new Command();
 
 program
-.version('1.0.0')
+  .version('1.0.0')
   .description('Compares two configuration files and shows a difference.')
   .arguments('<filepath1> <filepath2>')
   .option('-f, --format <type>', 'output format', 'stylish')
--- src/index.js
+++ src/index.js
@@ -9,36 +9,36 @@ const getRawData = (filePath) => {
 };
 
 const genDiff = (data1, data2) => {
-    const unionKeys = _.union(_.keys(data1), _.keys(data2));
+  const unionKeys = _.union(_.keys(data1), _.keys(data2));
 
-    const result = _.sortBy(unionKeys).reduce((acc, key) => {
-      if (_.has(data1, key) && _.has(data2, key)) {
-        if (data1[key] === data2[key]) {
-          acc.push(`    ${key}: ${data1[key]}`);
-        }
+  const result = _.sortBy(unionKeys).reduce((acc, key) => {
+    if (_.has(data1, key) && _.has(data2, key)) {
+      if (data1[key] === data2[key]) {
+        acc.push(`    ${key}: ${data1[key]}`);
       }
-      if (_.has(data1, key) && _.has(data2, key)) {
-        if (data1[key] !== data2[key]) {
-          acc.push(`  - ${key}: ${data1[key]}`);
-          acc.push(`  + ${key}: ${data2[key]}`);
-        }
+    }
+    if (_.has(data1, key) && _.has(data2, key)) {
+      if (data1[key] !== data2[key]) {
+        acc.push(`  - ${key}: ${data1[key]}`);
+        acc.push(`  + ${key}: ${data2[key]}`);
       }
-      if (_.has(data1, key) && !_.has(data2, key)) {
-          acc.push(`  - ${key}: ${data1[key]}`);
-      }
-      if (!_.has(data1, key) && _.has(data2, key)) {
-          acc.push(`  + ${key}: ${data2[key]}`);
-      }
-      return acc;
+    }
+    if (_.has(data1, key) && !_.has(data2, key)) {
+      acc.push(`  - ${key}: ${data1[key]}`);
+    }
+    if (!_.has(data1, key) && _.has(data2, key)) {
+      acc.push(`  + ${key}: ${data2[key]}`);
+    }
+    return acc;
   }, []);
 
   return result.join('\n');
 };
 
-  export default (filePath1, filePath2) => {
-    const data1 = getRawData(filePath1);
-    const data2 = getRawData(filePath2);
-    const dataParse1 = JSON.parse(data1);
-    const dataParse2 = JSON.parse(data2);
-    return genDiff(dataParse1, dataParse2);
-  };
+export default (filePath1, filePath2) => {
+  const data1 = getRawData(filePath1);
+  const data2 = getRawData(filePath2);
+  const dataParse1 = JSON.parse(data1);
+  const dataParse2 = JSON.parse(data2);
+  return genDiff(dataParse1, dataParse2);
+};
