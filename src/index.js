464db620f50fb8c51a1fcbb564cc274d87161fdd
Artem <selfex9496@gmail.com>
2023-02-20 16:44:32 +0700

    add parse function

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import { readFileSync } from 'fs';
 import { cwd } from 'process';
 import path from 'path';
 import _ from 'lodash';
+import parse from './parsers.js'
 
 const getRawData = (filepath) => {
   const fullPath = path.resolve(cwd(), filepath);
@@ -36,9 +37,11 @@ const getDiff = (data1, data2) => {
 };
 
 export default (filePath1, filePath2) => {
+  const fileExtension1 = path.extname(filePath1);
+  const fileExtension2 = path.extname(filePath2);
   const data1 = getRawData(filePath1);
   const data2 = getRawData(filePath2);
-  const dataParse1 = JSON.parse(data1);
-  const dataParse2 = JSON.parse(data2);
+  const dataParse1 = parse(data1, fileExtension1);
+  const dataParse2 = parse(data2, fileExtension2);
   return getDiff(dataParse1, dataParse2);
 };
