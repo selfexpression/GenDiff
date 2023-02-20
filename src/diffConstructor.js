98d28019ebf9fd2ced92889e7b5706c99eebdab6
Artem <selfex9496@gmail.com>
2023-02-20 18:03:00 +0700

    reinstall js-yaml, new module diffConstructor

---
+++ src/diffConstructor.js
@@ -0,0 +1,28 @@
+import _ from 'lodash';
+
+export default (data1, data2) => {
+  const unionKeys = _.union(_.keys(data1), _.keys(data2));
+
+  const result = _.sortBy(unionKeys).reduce((acc, key) => {
+    if (_.has(data1, key) && _.has(data2, key)) {
+      if (data1[key] === data2[key]) {
+        acc.push(`    ${key}: ${data1[key]}`);
+      }
+    }
+    if (_.has(data1, key) && _.has(data2, key)) {
+      if (data1[key] !== data2[key]) {
+        acc.push(`  - ${key}: ${data1[key]}`);
+        acc.push(`  + ${key}: ${data2[key]}`);
+      }
+    }
+    if (_.has(data1, key) && !_.has(data2, key)) {
+      acc.push(`  - ${key}: ${data1[key]}`);
+    }
+    if (!_.has(data1, key) && _.has(data2, key)) {
+      acc.push(`  + ${key}: ${data2[key]}`);
+    }
+    return acc;
+  }, []);
+
+  return result.join('\n');
+};
--- src/index.js
+++ src/index.js
@@ -1,39 +1,12 @@
 import { readFileSync } from 'fs';
 import { cwd } from 'process';
 import path from 'path';
-import _ from 'lodash';
-import parse from './parsers.js'
+import diffConstructor from './diffConstructor.js';
+import parse from './parsers.js';
 
 const getRawData = (filepath) => {
   const fullPath = path.resolve(cwd(), filepath);
   return readFileSync(fullPath, 'utf-8');
-};
-
-const getDiff = (data1, data2) => {
-  const unionKeys = _.union(_.keys(data1), _.keys(data2));
-
-  const result = _.sortBy(unionKeys).reduce((acc, key) => {
-    if (_.has(data1, key) && _.has(data2, key)) {
-      if (data1[key] === data2[key]) {
-        acc.push(`    ${key}: ${data1[key]}`);
-      }
-    }
-    if (_.has(data1, key) && _.has(data2, key)) {
-      if (data1[key] !== data2[key]) {
-        acc.push(`  - ${key}: ${data1[key]}`);
-        acc.push(`  + ${key}: ${data2[key]}`);
-      }
-    }
-    if (_.has(data1, key) && !_.has(data2, key)) {
-      acc.push(`  - ${key}: ${data1[key]}`);
-    }
-    if (!_.has(data1, key) && _.has(data2, key)) {
-      acc.push(`  + ${key}: ${data2[key]}`);
-    }
-    return acc;
-  }, []);
-
-  return result.join('\n');
 };
 
 export default (filePath1, filePath2) => {
@@ -43,5 +16,5 @@ export default (filePath1, filePath2) => {
   const data2 = getRawData(filePath2);
   const dataParse1 = parse(data1, fileExtension1);
   const dataParse2 = parse(data2, fileExtension2);
-  return getDiff(dataParse1, dataParse2);
+  return diffConstructor(dataParse1, dataParse2);
 };
