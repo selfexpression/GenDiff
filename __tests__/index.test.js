08566efab8744d2913105e4f9f90c860c6520562
Artem <selfex9496@gmail.com>
2023-02-24 09:31:44 +0700

    add new files

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -12,12 +12,16 @@ const readFile = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
 
 const expectedStylish = readFile('expectedStylish.txt');
 const expectedJSON = readFile('expectedJSON.txt');
+const expectedPlain = readFile('expectedPlain.txt');
 
 test('gendiff', () => {
   const filePathYAML1 = getFixturePath('file1.yml');
   const filePathYAML2 = getFixturePath('file2.yml');
   const filePathJSON1 = getFixturePath('file1.json');
   const filePathJSON2 = getFixturePath('file2.json');
+  const filePathTXT1 = getFixturePath('file1.txt');
+  const filePathTXT2 = getFixturePath('file2.txt');
   expect(genDiff(filePathYAML1, filePathYAML2)).toBe(expectedStylish);
   expect(genDiff(filePathJSON1, filePathJSON2)).toBe(expectedJSON);
+  expect(genDiff(filePathTXT1, filePathTXT2)).toBe(expectedPlain);
 });
